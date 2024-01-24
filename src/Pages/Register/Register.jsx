439072f2a3c3a9ac32fe422d010784326e23764c
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-01-25 00:16:10 +0330

    create register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './login.css'
 import './../../Style/log-register.css'
 import Topbar from '../../Components/Topbar/Topbar';
 import Footer from '../../Components/Footer/Footer';
@@ -7,18 +6,21 @@ import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 import Input from '../../Components/From/Input';
 import { Link } from 'react-router-dom';
+import { MdEmail } from "react-icons/md";
 
-export default function Login() {
+export default function Register() {
   return (
     <div>
         <Topbar/>
         <div className="login">
                 <img src="/image/aboutcar-2.jpeg" alt="login image" className='login-img'/>
                 <div className='login-bg'></div>
+                
+
             <div className='login-wrap'>
                <div className='login-header'>
                <h2>Welcome My Drive</h2>
-                <span>Login</span>
+                <span>Register</span>
                </div>
                <div className='form-wrap'>
                 <form >
@@ -27,7 +29,23 @@ export default function Login() {
                     <Input
                       element='input'
                       type='text'
+                      placeholder='enter your name'
+                    />
+                    </div>
+                    <div className='input-wrap'>
+                    <FaUser className='login-icon'/>
+                    <Input
+                      element='input'
+                      type='text'
                       placeholder='enter your username'
+                    />
+                    </div>
+                    <div className='input-wrap'>
+                    <MdEmail className='login-icon'/>
+                    <Input
+                      element='input'
+                      type='email'
+                      placeholder='enter your email'
                     />
                     </div>
                     <div className='input-wrap'>
@@ -41,10 +59,11 @@ export default function Login() {
                     <button className='login-btn'>login</button>
                 </form>
                 <div className='login-info'>
-                  <p>Have you already registered? <Link to='/register' className='log-link'>register</Link></p>
+                  <p>Do you have an account already? <Link to='/login' className='log-link'>login</Link></p>
                 </div>
                </div>
             </div>
+               
         </div>
     
         <Footer/>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Home from "./Pages/Home/Home"
 import Login from "./Pages/Login/Login"
+import Register from "./Pages/Register/Register"
 
 const routers=[
     {path:'/',element:<Home/>},
-    {path:'/login',element:<Login/>}
+    {path:'/login',element:<Login/>},
+    {path:'/register',element:<Register/>},
 
 ]
 
