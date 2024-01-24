747bcf12d70abfba62c6477bc48501b9e1f1034c
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-01-24 21:55:18 +0330

    login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import './login.css'
 import Topbar from '../../Components/Topbar/Topbar';
 import Footer from '../../Components/Footer/Footer';
-import HeaderSection from '../../Components/HeaderSection/HeaderSection';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 export default function Login() {
   return (
     <div>
         <Topbar/>
         <div className="login">
-            <div>
-               <div>
+                <img src="/image/aboutcar-2.jpeg" alt="login image" className='login-img'/>
+                <div className='login-bg'></div>
+            <div className='login-wrap'>
+               <div className='login-header'>
                <h2>Welcome My Drive</h2>
                 <span>Login</span>
                </div>
-               <div>
+               <div className='form-wrap'>
                 <form >
-                    <div>
-                    <i class="fa-solid fa-user"></i>
+                    <div className='input-wrap'>
+                    <FaUser className='login-icon'/>
                     <input type="text" placeholder='enter your username' />
                     </div>
+                    <div className='input-wrap'>
+                    <FaLock className='login-icon'/>
+                    <input type="text" placeholder='enter your username' />
+                    </div>
+                    <button>login</button>
                 </form>
                </div>
             </div>
