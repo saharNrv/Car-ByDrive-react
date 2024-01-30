2b94c356fe19ef5aa56148cfe0a733f83f8ee25f
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-01-30 22:48:04 +0330

    responsive car page

--- src/Pages/Car/Car.jsx
+++ src/Pages/Car/Car.jsx
@@ -10,10 +10,10 @@ export default function Car() {
       <div className='specs-car'>
          <div className="container-grid">
             <div className="row-grid">
-                <div className="col-6 ">
+                <div className="col-12 col-xs-12 col-md-6">
                     <img src="/image/car-1.webp" alt="car img" className='car-img' />
                 </div>
-                <div className="col-6">
+                <div className="col-12 col-xs-12 col-md-6">
                     <div className="car-info">
                         <h2 className='car-info-title'>Mercedes</h2>
                         <p className='car-info-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, adipisci! Dicta quos, sunt non architecto voluptates enim odio sequi tempora atque libero quas harum eligendi ad. Non ipsum provident iste.</p>
