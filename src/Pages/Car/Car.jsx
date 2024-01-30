46785c08ac1bc8bf6104f5e7963c461c4e90ccb7
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-01-30 16:52:54 +0330

    creat car page

---
+++ src/Pages/Car/Car.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Topbar from '../../Components/Topbar/Topbar';
+import Footer from '../../Components/Footer/Footer';
+import './car.css'
+
+export default function Car() {
+  return (
+    <div>
+        <Topbar/>
+      <div className='specs-car'>
+         <div className="container-grid">
+            <div className="row-grid">
+                <div className="col-6 ">
+                    <img src="/image/car-1.webp" alt="car img" className='car-img' />
+                </div>
+                <div className="col-6">
+                    <div className="car-info">
+                        <h2 className='car-info-title'>Mercedes</h2>
+                        <p className='car-info-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, adipisci! Dicta quos, sunt non architecto voluptates enim odio sequi tempora atque libero quas harum eligendi ad. Non ipsum provident iste.</p>
+                        <div className='car-pb'>
+                            <p className='car-price'>100000$</p>
+                            <button className='car-btn'>Add To Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
+      </div>
+      <Footer/>
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
+import Car from "./Pages/Car/Car"
 import Home from "./Pages/Home/Home"
 import Login from "./Pages/Login/Login"
 import Register from "./Pages/Register/Register"
 
 const routers=[
     {path:'/',element:<Home/>},
+    {path:'/car/mercedes',element:<Car/>},
     {path:'/login',element:<Login/>},
     {path:'/register',element:<Register/>},
 
