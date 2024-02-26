646219fbb9fcac649e5f1e109fd0ccf3a33821a3
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-02-26 19:37:11 +0330

    Filter products in menubar

--- src/Pages/AllCars/AllCars.js
+++ src/Pages/AllCars/AllCars.js
@@ -8,11 +8,13 @@ import CarBox from '../../Components/CarBox/CarBox';
 
 import './allcars.css'
 export default function AllCars() {
- 
+
   const [allCars, setAllCars] = useState([])
   const [orderAllCars, setOrderAllCars] = useState([])
   const [changeCarType, setChangeCarType] = useState('row')
-  const [searchVal,setSearchVal]=useState('')
+  const [status, setStatus] = useState('default')
+  const [statusTitle, setStatusTitle] = useState('all')
+  const [searchVal, setSearchVal] = useState('')
 
 
   useEffect(() => {
@@ -25,9 +27,38 @@ export default function AllCars() {
       })
   }, [])
 
-  const changeHandler=(e)=>{
+  useEffect(() => {
+    switch (status) {
+      case 'red': {
+        const redFilter = allCars.filter(car => car.color === 'red')
+        console.log(redFilter);
+        setOrderAllCars(redFilter)
+        break
+      }
+      case 'blue': {
+        const blueFilter = allCars.filter(car => car.color === 'blue')
+        setOrderAllCars(blueFilter)
+        break
+      }
+      case 'black': {
+        const blackFilter = allCars.filter(car => car.color === 'black')
+        setOrderAllCars(blackFilter)
+        break
+      }
+      case 'expensive': {
+        const redFilter = allCars.filter(car => car.price > 230000000)
+        setOrderAllCars(redFilter)
+        break
+      }
+      default: {
+        setOrderAllCars(allCars)
+      }
+    }
+  }, [status])
+
+  const changeHandler = (e) => {
     setSearchVal(e.target.value)
-    const filteredCars=allCars.filter(car=>car.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    const filteredCars = allCars.filter(car => car.name.toLowerCase().includes(e.target.value.toLowerCase()))
     setOrderAllCars(filteredCars)
 
   }
@@ -42,62 +73,85 @@ export default function AllCars() {
           <div className="menuBar">
             <div className='menuBar-right'>
               <div className='search-box'>
-                <input 
-                type="text"
-                 placeholder='search here...' 
-                 value={searchVal}
-                 onChange={changeHandler}
-                 />
+                <input
+                  type="text"
+                  placeholder='search here...'
+                  value={searchVal}
+                  onChange={changeHandler}
+                />
                 <FaSearch className='search-icon' />
               </div>
             </div>
             <div className='menuBar-left'>
-              <div className={`typeClass ${changeCarType==='row'?'active':''} `} onClick={()=>setChangeCarType('row')}>
+              <div className={`typeClass ${changeCarType === 'row' ? 'active' : ''} `} onClick={() => setChangeCarType('row')}>
                 <PiTextColumnsBold />
               </div>
-              <div className={`typeClass ${changeCarType==='cloumn'?'active':''} `} onClick={()=>setChangeCarType('cloumn')}>
+              <div className={`typeClass ${changeCarType === 'cloumn' ? 'active' : ''} `} onClick={() => setChangeCarType('cloumn')}>
                 <PiRowsBold />
+              </div>
+              <div className='allcars-top-bar__selection'>
+                <span className='allcars-title'>{statusTitle}</span>
+                <ul className='allcars-top-bar__selection-list'>
+                  <li className='allcars-top-bar__selection-item allcars-top-bar__selection-item--active'>All</li>
+                  <li className='allcars-top-bar__selection-item ' onClick={(event) => {
+                    setStatus('red')
+                    setStatusTitle(event.target.textContent)
+                  }}>red</li>
+                  <li className='allcars-top-bar__selection-item ' onClick={(event) => {
+                    setStatus('blue')
+                    setStatusTitle(event.target.textContent)
+                  }}>blue</li>
+                  <li className='allcars-top-bar__selection-item ' onClick={(event) => {
+                    setStatus('black')
+                    setStatusTitle(event.target.textContent)
+                  }}>black</li>
+                  <li className='allcars-top-bar__selection-item ' onClick={(event) => {
+                    setStatus('expensive')
+                    setStatusTitle(event.target.textContent)
+                  }}>expensive</li>
+
+                </ul>
               </div>
             </div>
           </div>
           {/* courses */}
           {
-            changeCarType==='row'?(
+            changeCarType === 'row' ? (
               <>
-          <div className="row-grid">
-            {
-              orderAllCars.map(car => (
+                <div className="row-grid">
+                  {
+                    orderAllCars.map(car => (
 
 
-                <CarBox {...car} />
-              ))
-            }
-          </div>
+                      <CarBox {...car} />
+                    ))
+                  }
+                </div>
               </>
 
-            ):(
+            ) : (
               <>
-               {
-              orderAllCars.map(car => (
+                {
+                  orderAllCars.map(car => (
 
 
-          <div className="cloumn-car">
-            <div className='cloumn'>
-              <img src={car.img} alt="car" />
-              <div className='info-box'>
-                <h2>{car.name}</h2>
-              <p>
-                 {car.info}
-              </p>
-              <span>{car.price}</span>
+                    <div className="cloumn-car">
+                      <div className='cloumn'>
+                        <img src={car.img} alt="car" />
+                        <div className='info-box'>
+                          <h2>{car.name}</h2>
+                          <p>
+                            {car.info}
+                          </p>
+                          <span>{car.price}</span>
 
-              </div>
-              <button>see more</button>
-            </div>
-          </div>
-                
-              ))
-            }
+                        </div>
+                        <button>see more</button>
+                      </div>
+                    </div>
+
+                  ))
+                }
               </>
             )
           }
